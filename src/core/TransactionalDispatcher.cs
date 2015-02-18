16563f1100d912b01a7144e44c1bae247d7a7279
Jon Bradford <jon.bradford@bathasu.com>
2015-02-18 09:40:54 +0000

    Added Transactional Dispatcher

---
+++ src/core/TransactionalDispatcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace CR.MessageDispatch.Core
+{
+    class TransactionalDispatcher<TMessage>:IDispatcher<TMessage>
+    {
+        private readonly IDispatcher<TMessage> _dispatcher;
+
+        public TransactionalDispatcher(IDispatcher<TMessage> dispatcher)
+        {
+            _dispatcher = dispatcher;
+        }
+
+        public void Dispatch(TMessage message)
+        {
+            using (var transaction = new TransactionScope())
+            {
+                _dispatcher.Dispatch(message);
+                transaction.Complete();
+            }
+        }
+    }
+}
