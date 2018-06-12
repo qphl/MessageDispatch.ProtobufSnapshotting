7673d2ea970f0a642b2da48ef7ddb5cb2c1560d8
Isaac <23095702+iBeizsley@users.noreply.github.com>
2018-06-12 16:46:31 +0100

    Made repository a single package

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ version="0.0.0"
 if [ -n "$1" ]; then version="$1"
 fi
 
-dotnet pack src -o ../../dist -p:Version="$version" -p:PackageVersion="$version" -c Release
+dotnet pack src/protobufSnapshotting/protobufSnapshotting.csproj -o ../../dist -p:Version="$version" -p:PackageVersion="$version" -c Release
