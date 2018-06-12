fe485397ed6bc9697f401328267675bf89a9cba7
Isaac <23095702+iBeizsley@users.noreply.github.com>
2018-06-12 10:53:13 +0100

    Pass in just full version to build scripts

--- build.cmd
+++ build.cmd
@@ -1,21 +1,6 @@
 @echo off
 
-SET MAJOR=0
-IF NOT [%1]==[] (set MAJOR=%1)
+SET VERSION=0.0.0
+IF NOT [%1]==[] (set VERSION=%1)
 
-SET MINOR=0
-IF NOT [%2]==[] (set MINOR=%2)
-
-set PATCH=0
-IF NOT [%3]==[] (set PATCH=%3)
-
-set VERSION=%MAJOR%.%MINOR%.%PATCH%
-
-set SUFFIX=
-IF NOT [%4]==[] (set SUFFIX=%4)
-
-set FULLVERSION=%VERSION%
-
-IF NOT [%SUFFIX%]==[] (set FULLVERSION="%FULLVERSION%-%SUFFIX%")
-
-dotnet pack src -o ../../dist -p:Version=%VERSION% -p:PackageVersion=%FULLVERSION% -c Release
+dotnet pack src -o ../../dist -p:Version="%VERSION%" -p:PackageVersion="%VERSION%" -c Release
--- build.sh
+++ build.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
-major="0"
-if [ -n "$1" ]; then major="$1"
+version="0.0.0"
+if [ -n "$1" ]; then version="$1"
 fi
 
-minor="0"
-if [ -n "$2" ]; then minor="$2"
-fi
-
-patch="0"
-if [ -n "$3" ]; then patch="$3"
-fi
-
-version="$major"".""$minor"".""$patch"
-
-versionSuffix=""
-if [ -n "$4" ]; then versionSuffix="$4"
-fi
-
-fullVersion="$version"
-if [ -n "$versionSuffix" ]; then fullVersion="$fullVersion-$versionSuffix"
-fi
-
-dotnet pack src -o ../../dist -p:Version="$version" -p:PackageVersion="$fullVersion" -c Release
+dotnet pack src -o ../../dist -p:Version="$version" -p:PackageVersion="$version" -c Release
