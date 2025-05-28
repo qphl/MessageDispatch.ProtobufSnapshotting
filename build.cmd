b4db3a08c753c426b1216c44a7c5768addf691b5
Josh Pattie <josh.pattie@qphl.co>
2025-05-28 15:12:44 +0100

    Reloacted style files and update build script

--- build.cmd
+++ build.cmd
@@ -7,4 +7,9 @@ SET TAG=0.0.0
 IF NOT [%2]==[] (set TAG=%2)
 SET TAG=%TAG:tags/=%
 
+dotnet restore .\src\PharmaxoScientific.MessageDispatch.ProtobufSnapshotting.sln -PackagesDirectory .\src\packages -Verbosity detailed
+
+dotnet format .\src\PharmaxoScientific.MessageDispatch.ProtobufSnapshotting.sln --severity warn --verify-no-changes -v diag
+IF %errorlevel% neq 0 EXIT /B %errorlevel%
+
 dotnet pack .\src\protobufSnapshotting\protobufSnapshotting.csproj -o .\dist -p:Version="%VERSION%" -p:PackageVersion="%VERSION%" -p:Tag="%TAG%" -c Release
