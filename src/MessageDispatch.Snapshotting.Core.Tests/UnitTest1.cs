922a8e8e0a270ff3961b3010b18773a97554cd13
Sam Matthews <sammatthews@hotmail.com>
2025-11-13 11:40:40 +0000

    Add new projects

---
+++ src/MessageDispatch.Snapshotting.Core.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MessageDispatch.Snapshotting.Core.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ src/MessageDispatch.Snapshotting.Core/ISnapshotStrategy.cs
@@ -0,0 +1,8 @@
+﻿// Copyright (c) Pharmaxo. All rights reserved.
+
+namespace PharmaxoScientific.MessageDispatch.Snapshotting.Core;
+
+public interface ISnapshotStrategy<T>
+{
+    bool ShouldSnapshotForEvent(T @event);
+}
