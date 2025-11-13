4e02a436ee731470d304a3c2bd9ea55609219940
Sam Matthews <sammatthews@hotmail.com>
2025-11-13 15:55:10 +0000

    NamedEventSnapshotStrategy takes configurable event type

---
+++ src/MessageDispatch.Snapshotting.Core.Tests/NamedEventSnapshotStrategyTests.cs
@@ -0,0 +1,26 @@
+using PharmaxoScientific.MessageDispatch.Snapshotting.Core;
+
+namespace MessageDispatch.Snapshotting.Core.Tests;
+
+public class NamedEventSnapshotStrategyTests
+{
+    [TestCase("CheckpointRequested")]
+    [TestCase("DoSnapshotNowPlease")]
+    public void ShouldSnapshotForEvent_GivenMatchingEventType_ReturnsTrue(string snapshottingEventType)
+    {
+        var strategy = new NamedEventSnapshotStrategy(snapshottingEventType);
+        var resolvedEvent = TestHelpers.BuildResolvedEvent(snapshottingEventType);
+
+        Assert.That(strategy.ShouldSnapshotForEvent(resolvedEvent), Is.True);
+    }
+
+    [TestCase("CheckpointPlease")]
+    [TestCase("CheckpointNotRequested")]
+    public void ShouldSnapshotForEvent_GivenNonMatchingEventType_ReturnsFalse(string nonSnapshottingEventType)
+    {
+        var strategy = new NamedEventSnapshotStrategy("CheckpointRequested");
+        var resolvedEvent = TestHelpers.BuildResolvedEvent(nonSnapshottingEventType);
+
+        Assert.That(strategy.ShouldSnapshotForEvent(resolvedEvent), Is.False);
+    }
+}
---
+++ src/MessageDispatch.Snapshotting.Core/NamedEventSnapshotStrategy.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Pharmaxo. All rights reserved.
+
+using KurrentDB.Client;
+
+namespace PharmaxoScientific.MessageDispatch.Snapshotting.Core;
+
+public class NamedEventSnapshotStrategy : ISnapshotStrategy<ResolvedEvent>
+{
+    private readonly string _eventTypeToSnapshotOn;
+
+    public NamedEventSnapshotStrategy(string eventTypeToSnapshotOn) => _eventTypeToSnapshotOn = eventTypeToSnapshotOn;
+
+    public bool ShouldSnapshotForEvent(ResolvedEvent @event) => @event.Event.EventType == _eventTypeToSnapshotOn;
+}
