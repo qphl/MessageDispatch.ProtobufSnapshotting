2472219def71a5b8537f5d3f4e11797134f76ec4
Sam Matthews <sammatthews@hotmail.com>
2025-11-14 11:43:30 +0000

    Fix build.cmd

--- build.cmd
+++ build.cmd
@@ -7,9 +7,9 @@ SET TAG=0.0.0
 IF NOT [%2]==[] (set TAG=%2)
 SET TAG=%TAG:tags/=%
 
-dotnet restore .\src\PharmaxoScientific.MessageDispatch.ProtobufSnapshotting.sln -PackagesDirectory .\src\packages -Verbosity detailed
+dotnet restore .\src\PharmaxoScientific.MessageDispatch.Snapshotting.sln -PackagesDirectory .\src\packages -Verbosity detailed
 
-dotnet format .\src\PharmaxoScientific.MessageDispatch.ProtobufSnapshotting.sln --severity warn --verify-no-changes -v diag
+dotnet format .\src\PharmaxoScientific.MessageDispatch.Snapshotting.sln --severity warn --verify-no-changes -v diag
 IF %errorlevel% neq 0 EXIT /B %errorlevel%
 
-dotnet pack .\src\MessageDispatch.ProtobufSnapshotting\MessageDispatch.ProtobufSnapshotting.csproj -o .\dist -p:Version="%VERSION%" -p:PackageVersion="%VERSION%" -p:Tag="%TAG%" -c Release
+dotnet pack .\src\MessageDispatch.Snapshotting\MessageDispatch.Snapshotting.csproj -o .\dist -p:Version="%VERSION%" -p:PackageVersion="%VERSION%" -p:Tag="%TAG%" -c Release
