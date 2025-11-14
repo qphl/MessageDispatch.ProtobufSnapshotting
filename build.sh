359a75db596aa1ebe94d226ac194ae8fc4a65780
Sam Matthews <sammatthews@hotmail.com>
2025-11-14 12:52:12 +0000

    Fix (already broken) shell build script

--- build.sh
+++ build.sh
@@ -9,4 +9,6 @@ if [ -n "$2" ]; then tag="$2"
 fi
 tag=${tag/tags\//}
 
-dotnet pack .\\src\\protobufSnapshotting\\protobufSnapshotting.csproj -o .\\dist -p:Version="$version" -p:PackageVersion="$version" -p:Tag="$tag" -c Release
+dotnet pack .\\src\\MessageDispatch.Snapshotting.Core\\MessageDispatch.Snapshotting.Core.csproj -o .\\dist -p:Version="$version" -p:PackageVersion="$version" -p:Tag="$tag" -c Release
+dotnet pack .\\src\\MessageDispatch.Snapshotting.Json\\MessageDispatch.Snapshotting.Json.csproj -o .\\dist -p:Version="$version" -p:PackageVersion="$version" -p:Tag="$tag" -c Release
+dotnet pack .\\src\\MessageDispatch.Snapshotting.Protobuf\\MessageDispatch.Snapshotting.Protobuf.csproj -o .\\dist -p:Version="$version" -p:PackageVersion="$version" -p:Tag="$tag" -c Release
